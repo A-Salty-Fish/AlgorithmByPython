5c04a79fc5c915c27d58c66ddc0707287f530d45
A-Salty-Fish <1309032417@qq.com>
2020-08-23 22:14:30 +0800

    2n+2个数中落单的2个数

---
+++ 25.SingleNumberIII.py
@@ -0,0 +1,12 @@
+def SingleNumberIII(A):
+    s = 0
+    for x in A:
+        s ^= x
+    y = s & (-s)
+    ans = [0, 0]
+    for x in A:
+        if (x & y) != 0:
+            ans[0] ^= x
+        else:
+            ans[1] ^= x
+    return ans
